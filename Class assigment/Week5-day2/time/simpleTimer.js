7f35a535a3be57b1bb7de3ebf49afb8e5948aac4
LrE <lrespino@gmail.com>
2019-05-14 22:44:22 -0400

    index setup

---
+++ Class assigment/Week5-day2/time/simpleTimer.js
@@ -0,0 +1,43 @@
+// Step 1:
+// Use the following Audio file below:
+
+var audio = new Audio("raven.mp3");
+
+//  Step 2: do the following between the --- comments below
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
+
+// ---------------------
+// CODE STEP TWO HERE
+// ---------------------
+setTimeout(fiveSeconds,5000);
+setTimeout(tenSeconds,10000);
+setTimeout(timeUp,15000);
+// Step 3:
+// Fill in the blanks to these functions.
+function fiveSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  $("#time-left").append("<h2> About 10 seconds left</h2>")
+  console.log("10 seconds left")
+  // console log 10 seconds left
+  // return audio.play("raven.mp3");
+}
+
+function tenSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  $("#time-left").append("<h2> About 5 seconds left</h2>")
+console.log("5 seconds left")
+ return audio.play("raven.mp3");
+}
+
+function timeUp() {
+  // in the element with an id of `time-left` add an h2 saying Time's Up!
+  // console log done
+  console.log("Done")
+
+  $("#time-left").append("<h2> Time's Up!</h2>")
+  // The following line will play the audio file above
+  return audio.play("raven.mp3");
+}
+
---
+++ Class assigment/Week6-Day1/averages.js
@@ -0,0 +1,10 @@
+function averages (numArr) {
+    var sum = 0;
+    for (var i = 0; i <numArr.lenght; i++){
+    sum =sum + numArr[i];
+
+    }
+
+    return sum / numArr.lenght;
+
+}
